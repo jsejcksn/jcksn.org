18e80d70512836978c5d5c00a0903c07ba1a529c
Jesse Jackson <jsejcksn@users.noreply.github.com>
2017-10-03 14:46:14 -0500

    Initial commit

---
+++ jcksn.js
@@ -0,0 +1,20 @@
+// Jackson - jcksn.org
+
+// Variables ----------
+
+
+
+
+// Functions ----------
+
+
+
+
+// EventListeners ----------
+
+
+
+
+// Execute ----------
+
+console.log('Welcome to jcksn.org');
